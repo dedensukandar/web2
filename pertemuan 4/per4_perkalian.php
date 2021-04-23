ab7bcbca65bf8c2116cab4b2113d20b27357f4d7
dedensukandar <82696719+dedensukandar@users.noreply.github.com>
2021-04-23 22:00:48 +0700

    Pertemuan 4 Pemrograman Web

---
+++ pertemuan 4/per4_perkalian.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>Perkalian</title>
+</head>
+<body>
+
+  <h2>Tabel Perkalian</h2>
+  ============================= <br>
+  <?php
+  $a1 = 12;
+  for ($b = 15; $b <= 45; $b+=2) {
+    $jml = $a1 * $b;
+    echo "<p> $a1 * $b = $jml";
+  }
+  ?>
+
+</body>
+
+</html>
