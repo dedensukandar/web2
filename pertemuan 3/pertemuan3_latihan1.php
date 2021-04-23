a4faf27292035dd464e3ec1ae4cea8b40202f5e7
dedensukandar <82696719+dedensukandar@users.noreply.github.com>
2021-04-23 17:30:14 +0700

    Update pertemuan3_latihan1.php

--- pertemuan 3/pertemuan3_latihan1.php
+++ pertemuan 3/pertemuan3_latihan1.php
@@ -1,23 +1,24 @@
-<? 
-$ball = 'purple'; 
- //Coba ubah ke "yellow", "blue", "green", "purple", atau warna lain 
-if ($ball == 'red') { 
- $redbox = $ball; 
-} elseif ($ball == 'yellow') { 
- $yellowbox = $ball; 
-} elseif ($ball == 'blue') { 
- $bluebox = $ball; 
-} elseif ($ball == 'green') { 
- $greenbox = $ball; 
-} elseif ($ball == 'purple') { 
- $purplebox = $ball; 
-} else { 
- $colorlessbox = $ball; 
-} 
-echo "red box : $redbox <br>\n"; 
-echo "yellow box : $yellowbox <br>\n"; 
-echo "blue box : $bluebox <br>\n"; 
-echo "green box : $greenbox <br>\n"; 
-echo "purple box : $purplebox <br>\n"; 
-echo "colorless box : $colorlessbox <br>\n"; 
-?>
+<?php
+
+$ball = "blue";
+ //Coba ubah ke "yellow", "blue", "green", "purple", atau warna lain
+if ($ball == "red") {
+ $redbox = $ball;
+	echo "red box : $redbox <br>";
+} elseif ($ball == "yellow") {
+ $yellowbox = $ball;
+	echo "yellow box : $yellowbox <br>";
+} elseif ($ball == "blue") {
+ $bluebox = $ball;
+	echo "blue box : $bluebox <br>";
+} elseif ($ball == "green") {
+ $greenbox = $ball;
+	echo "green box : $greenbox <br>";
+} elseif ($ball == "purple") {
+ $purplebox = $ball;
+	echo "purple box : $purplebox <br>";
+} else {
+ $colorlessbox = $ball;
+	echo "colorless box : $colorlessbox <br>";
+}
+?> 
